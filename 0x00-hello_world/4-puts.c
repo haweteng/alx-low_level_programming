47efb3f4ef1ae20eb495a52941c8f0ae31281b10
Haweten <hawetengirma27@gmail.com>
2023-04-25 02:25:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
-*main - prints "Programming is like building a multilingual puzzle, followed by a new line."
-*return 0.
+*main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+*Return: Always 0.
 */
 
 
