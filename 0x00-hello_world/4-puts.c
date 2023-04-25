a84621ff6e35a842c8df46e9cb6b1280b8ff582d
Haweten <hawetengirma27@gmail.com>
2023-04-25 02:19:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
+
 /**
-*main - entry point
-*return: always 0 (success)
+*main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+*return 0.
 */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
