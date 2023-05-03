e4c3e76e12d5e54eb022f002184ef9b726edf37e
Haweten <hawetengirma27@gmail.com>
2023-05-03 05:54:01 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,19 @@
 void print_rev(char *s)
 
 {
-	int c = 0;
+	int longi = 0;
+	int o;
 
-	while (s[c] != '\0')
+	while (*s != '\0')
 	{
-	c++;
+		longi++
+		s++;
 	}
-
-	for (c -= 1; c >= 0; c--)
+	s--;
+	for (o = longi; o > 0; o--)
 	{
-	_putchar(s[c]);
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }
